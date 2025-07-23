f6c7bcb2f23a3906673419c52b7e8a1a1237b82b
Rayan <rayanjaberi2001@gmail.com>
2025-07-23 14:32:35 +0330

    make prg without dbg

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 import socket
 
-def ipRangeScanner ():
+def ipRangeScanner (ipAdd):
     protocols = (80 , 8080 , 443 , 53 , 23 , 22 )
     s = socket.socket();
+    for i in protocols:
+        s.connect_ex((ipAdd , i))
+        s.close()
 
-
+userIp = input("enter ip address :")
+print(ipRangeScanner(userIp))
